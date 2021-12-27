f4ff105b8752fb199afc788f3212dfac5357739c
wirehead <wirehead@wireheadmechanist.com>
2021-12-26 21:08:33 -0600

    py fix

--- a1c.py
+++ a1c.py
@@ -1,11 +1,10 @@
 # function to calculate A1C
 def aonec():
 
-
-getbgc = input("gluc?")
-bgc = float(getbgc)
-atrans = (bgc + 46.7)
-aonec = (atrans / 28.7)
+    getbgc = input("gluc?")
+    bgc = float(getbgc)
+    atrans = (bgc + 46.7)
+    aonec = (atrans / 28.7)
 
     return(round(aonec,2))
 
